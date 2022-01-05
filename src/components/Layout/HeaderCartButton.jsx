22a8f3fc34689fd2ddea1c8f953270833a1ec7ef
spencer barriball <sbarriball@me.com>
2022-01-05 15:00:14 +0000

    update HeaderCartButton to show number of items using CartContext and reduce function

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import Carticon from '../Cart/Carticon';
-import classes from './HeaderCartButton.module.css';
+import React, { useContext } from "react";
+import Carticon from "../Cart/Carticon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from '../../store/CartContext';
 
+const HeaderCartButton = (props) => {
 
-const HeaderCartButton = props => {
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-            <span className={classes.icon}><Carticon /></span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
-            
-        </button>
-    )
-}
+  const cartCtx = useContext(CartContext)
 
-export default HeaderCartButton
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) =>{
+    return currentNumber + item.amount;
+  }, 0)
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <Carticon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;

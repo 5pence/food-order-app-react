dcc9111249e504cdd48583e82c4740ad01477bc8
spencer barriball <sbarriball@me.com>
2022-01-05 01:42:23 +0000

    initial commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from './Header.module.css';
+import mealsImages from '../../assets/meals.jpg';
+
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+          <h1>ReactMeals</h1>
+          <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+          <img src={mealsImages} alt="lovely food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
